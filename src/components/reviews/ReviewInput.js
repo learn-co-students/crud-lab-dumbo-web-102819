1bfa38a47b88045f90d51bc14b190d86010a3342
Dominic Chu <dominic.m.chu@gmail.com>
2020-02-28 13:23:41 -0500

    Done.

--- src/components/reviews/ReviewInput.js
+++ src/components/reviews/ReviewInput.js
@@ -1,12 +1,34 @@
 import React, { Component } from 'react';
-import Reviews from './Reviews';
 
 class ReviewInput extends Component {
+  state = {
+    text: ''
+  }
+  handleChange = (event) => {
+    this.setState({
+      text: event.target.value
+    })
+  }
+  handleSubmit = (event) => {
+    event.preventDefault()
+
+    const newReview = {
+      text: this.state.text, 
+      restaurantId: this.props.restaurantId
+    }
+
+    this.props.addReview(newReview)
+
+    this.setState({
+      text: ''
+    })
+  }
   render() {
     return (
-      <div>
-        Review Input
-      </div>
+        <form onSubmit={this.handleSubmit}>
+          <input type="text" value={this.state.reviewText} onChange={this.handleChange} /> 
+          <input type="submit" />
+        </form>
     );
   }
 };
--- src/components/reviews/Reviews.js
+++ src/components/reviews/Reviews.js
@@ -2,10 +2,15 @@ import React, { Component } from 'react';
 import Review from './Review';
 
 class Reviews extends Component {
+  mapAllReviews = () => {
+    return this.props.reviews.map(review => {
+      return <Review deleteReview={this.props.deleteReview} review={review} /> 
+    })
+  }
   render() {
     return (
       <ul>
-        Reviews
+        {this.mapAllReviews()}
       </ul>
     );
   }
